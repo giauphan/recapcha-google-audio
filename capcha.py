8609eca565852098f6f36beeac552029579b961c
giauphan <giauphan012@gmail.com>
2024-02-26 11:30:36 +0700

    fix screem

--- capcha.py
+++ capcha.py
@@ -62,7 +62,7 @@ def process_page_data(page):
 def bytedance():
     with sync_playwright() as p:
         browser = p.chromium.launch(headless=True, args=['--disable-gpu', '--single-process','--incognito'])
-        ctx =  browser.new_context(viewport= { 'width':  375, "height": 667})
+        ctx =  browser.new_context()
 
         for pagination in range(1, 5):
             page = ctx.new_page()
