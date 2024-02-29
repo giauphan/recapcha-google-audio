1e10efc222fb5251683d7bea60c6f08b03befb79
giauphan <giauphan012@gmail.com>
2024-02-29 17:04:56 +0700

    fix recapcha

--- downloadBcdn.py
+++ downloadBcdn.py
@@ -17,13 +17,15 @@ async def process_page_data(enterprise_code_text, page_find):
     await page_find.wait_for_timeout(5000)
     await page_find.locator("#ctl00_C_ANNOUNCEMENT_TYPE_IDFilterFld").select_option("NEW")
     await page_find.wait_for_timeout(5000)
+    await page_find.locator("#ctl00_C_ENT_GDT_CODEFld").click()
+    await page_find.locator("#ctl00_C_ENT_GDT_CODEFld").fill(f"{enterprise_code_text}")
+    await page_find.wait_for_timeout(5000)
 
     for _ in range(3):  
         try:
             challenger = AsyncChallenger(page_find)
             await challenger.solve_recaptcha()
-            await page_find.locator("#ctl00_C_ENT_GDT_CODEFld").click()
-            await page_find.locator("#ctl00_C_ENT_GDT_CODEFld").fill(f"{enterprise_code_text}")
+            
             await page_find.get_by_role("button", name="Tìm kiếm", exact=True).click()
             async with page_find.expect_download() as download_info:
                 await page_find.locator("#ctl00_C_CtlList_ctl02_LnkGetPDFActive").click()
@@ -48,7 +50,7 @@ async def process_page_data(enterprise_code_text, page_find):
 
 async def bytedance():
     async with async_playwright() as p:
-        browser = await p.chromium.launch(headless=True, args=["--single-process", "--incognito"])
+        browser = await p.chromium.launch(headless=False, args=["--single-process", "--incognito"])
         ctx = await browser.new_context()
         Electronic = await Electronic_report.objects.all()
 
