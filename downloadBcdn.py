d7367adccaba24d41182012183627250d7dd2287
giauphan <giauphan012@gmail.com>
2024-02-29 15:43:22 +0700

    wait load file

--- downloadBcdn.py
+++ downloadBcdn.py
@@ -44,8 +44,6 @@ async def process_page_data(arr_business_code, ctx):
         await page_find.get_by_role("button", name="Tìm kiếm", exact=True).click()
 
         async with page_find.expect_download() as download_info:
-            await page_find.wait_for_selector("#ctl00_C_CtlList_ctl02_LnkGetPDFActive")
-            await page_find.wait_for_timeout(10000)
             await page_find.locator("#ctl00_C_CtlList_ctl02_LnkGetPDFActive").click()
 
         file_name = f"{enterprise_code_text}.pdf"
