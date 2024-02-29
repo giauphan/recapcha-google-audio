309bb29ef3e30b991f7189491e9ea0b7714e9536
giauphan <giauphan@users.noreply.github.com>
2024-02-29 09:09:51 +0000

    chore: fix code style

--- downloadBcdn.py
+++ downloadBcdn.py
@@ -8,6 +8,7 @@ from Model.ElectronicReport import Electronic_report
 
 load_dotenv()
 
+
 async def process_page_data(arr_business_code, ctx):
     CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
     DOWNLOAD_DIR = os.path.join(CURRENT_DIR, "downloads")
@@ -22,7 +23,9 @@ async def process_page_data(arr_business_code, ctx):
         print(os.getenv("url_find_bcdn"))
         await page_find.wait_for_selector("#ctl00_C_ANNOUNCEMENT_TYPE_IDFilterFld")
         await page_find.wait_for_timeout(5000)
-        await page_find.locator("#ctl00_C_ANNOUNCEMENT_TYPE_IDFilterFld").select_option("NEW")
+        await page_find.locator("#ctl00_C_ANNOUNCEMENT_TYPE_IDFilterFld").select_option(
+            "NEW"
+        )
         await page_find.wait_for_timeout(5000)
 
         for _ in range(3):  # Try 3 times
@@ -30,10 +33,16 @@ async def process_page_data(arr_business_code, ctx):
                 challenger = AsyncChallenger(page_find)
                 await challenger.solve_recaptcha()
                 await page_find.locator("#ctl00_C_ENT_GDT_CODEFld").click()
-                await page_find.locator("#ctl00_C_ENT_GDT_CODEFld").fill(f"{enterprise_code_text}")
-                await page_find.get_by_role("button", name="Tìm kiếm", exact=True).click()
+                await page_find.locator("#ctl00_C_ENT_GDT_CODEFld").fill(
+                    f"{enterprise_code_text}"
+                )
+                await page_find.get_by_role(
+                    "button", name="Tìm kiếm", exact=True
+                ).click()
                 async with page_find.expect_download() as download_info:
-                    await page_find.locator("#ctl00_C_CtlList_ctl02_LnkGetPDFActive").click()
+                    await page_find.locator(
+                        "#ctl00_C_CtlList_ctl02_LnkGetPDFActive"
+                    ).click()
                 download = download_info.value
                 file_name = f"{enterprise_code_text}.pdf"
                 download_path = os.path.join(DOWNLOAD_DIR, file_name)
@@ -54,13 +63,17 @@ async def process_page_data(arr_business_code, ctx):
 
         await page_find.close()
 
+
 async def download_bcdn():
     async with async_playwright() as p:
-        browser = await p.chromium.launch(headless=True, args=["--single-process", "--incognito"])
+        browser = await p.chromium.launch(
+            headless=True, args=["--single-process", "--incognito"]
+        )
         ctx = await browser.new_context()
         Electronic = await Electronic_report.objects.all()
         await process_page_data(Electronic, ctx)
         await browser.close()
+
 
 if __name__ == "__main__":
     asyncio.run(download_bcdn())
